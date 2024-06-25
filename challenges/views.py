49603de33aea79534e03cc7cb0e799aea0b664ee
LuisFernandoLV25 <luisfernandolopezvarela1@gmail.com>
2024-06-25 11:46:46 -0600

    Proyecto practica Django

---
+++ challenges/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseNotFound
+
+
+def monthly_challenges(response,month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Jugar basquetball al menos dos veces a la semana"
+    elif month == "february":
+        challenge_text = "Ir al gym 6 veces a la semana"
+    elif month == "march":
+        challenge_text = "Caminar 20 minutos cada dia de la semana"
+    else:
+        return HttpResponseNotFound(challenge_text)
+    
+    return HttpResponse("Page not found")
