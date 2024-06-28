e3b5025aba81b0cebdfb7b6d4535993436471d7b
LuisFernandoLV25 <luisfernandolopezvarela1@gmail.com>
2024-06-27 19:42:50 -0600

    Plantillas y filtros

--- challenges/views.py
+++ challenges/views.py
@@ -1,29 +1,55 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
-    "january": "Jugar basquetball al menos dos veces a la semana",
-    "february" : "Ir al gym 6 veces a la semana",
-    "march": "Caminar 20 minutos cada dia de la semana",
-    "april": "Jugar basquetball al menos dos veces a la semana",
-    "may": "Ir al gym 6 veces a la semana",
-    "june": "Caminar 20 minutos cada dia de la semana",
-    "july": "Jugar basquetball al menos dos veces a la semana",
-    "august" : "Ir al gym 6 veces a la semana",
-    "september": "Caminar 20 minutos cada dia de la semana",
-    "october" : "Jugar basquetball al menos dos veces a la semana",
-    "november": "Ir al gym 6 veces a la semana",
-    "December": "Caminar 20 minutos cada dia de la semana"
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Eat no meat for the entire month!",
+    "may": "Walk for at least 20 minutes every day!",
+    "june": "Learn Django for at least 20 minutes every day!",
+    "july": "Eat no meat for the entire month!",
+    "august": "Walk for at least 20 minutes every day!",
+    "september": "Learn Django for at least 20 minutes every day!",
+    "october": "Eat no meat for the entire month!",
+    "november": "Walk for at least 20 minutes every day!",
+    "december": "Learn Django for at least 20 minutes every day!"
 }
 
+# Create your views here.
 
-def monthly_challenge_by_number(request,month):
-    return HttpResponse(month)
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
 
-def monthly_challenge(response,month):
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+
+    # "<li><a href="...">January</a></li><li><a href="...">February</a></li>..."
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+
+    redirect_month = months[month - 1]
+    redirect_path = reverse("month-challenge", args=[redirect_month]) # /challenge/january
+    return HttpResponseRedirect(redirect_path)
+
+
+def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
+        return render(request,"challenges/challenge.html",{
+            "text": challenge_text,
+            "month_name":month
+        })
     except:
-        return HttpResponseNotFound("This month is not supported")
-
-    return HttpResponse(challenge_text)
+        return HttpResponseNotFound("<h1>This month is not supported skere!</h1>")
