77893a1dcfe575e43ac384a81e7bfcb847dccddf
LuisFernandoLV25 <luisfernandolopezvarela1@gmail.com>
2024-06-29 20:58:31 -0600

    Fragmentos, Errores y herencia

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect,Http404
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january": "Eat no meat for the entire month!",
@@ -45,4 +46,4 @@ def monthly_challenge(request, month):
             "month_name":month
         })
     except:
-        return HttpResponseNotFound("<h1>This month is not supported skere!</h1>")
+        raise Http404()
